866415413bb504cca475c632c9dc506877328343
Rugved Thota <rugvedthota9@gmail.com>
2021-02-16 07:31:14 +0000

    Removed Popus Making Website Faster!

--- connect4/urls.py
+++ connect4/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from game import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -27,3 +29,5 @@ urlpatterns = [
     path('age/', views.age, name='age'),
     path('blog/', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
