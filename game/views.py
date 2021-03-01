1824815a9aa8120ccd6f8879f80beb08afe86ccf
Rugved Thota <rugvedthota9@gmail.com>
2021-03-01 10:56:24 +0000

    New Patch Update Added Wack The Mole

--- game/views.py
+++ game/views.py
@@ -9,8 +9,8 @@ def connect4(request):
 def tic(request):
     return render(request, 'tictac.html')
 
-def tennis(request):
-    return render(request, 'tennis.html')
+def w(request):
+    return render(request, 'wack.html')
 
 def privacy(request):
     return render(request, 'Privacy Policy.html')
