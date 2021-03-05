dcd0610d995b3b2677d71c2592354b1bc5aa41ba
Rugved Thota <rugvedthota9@gmail.com>
2021-03-05 15:33:14 +0000

    New Patch Update In Blog

--- game/models.py
+++ game/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    url = models.URLField(max_length=2048)
+    image = models.ImageField(upload_to='game/images', blank=True)
