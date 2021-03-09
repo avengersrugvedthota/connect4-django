032d9043355c78af53fb45028f1217823319d6cf
Rugved Thota <rugvedthota9@gmail.com>
2021-03-09 14:52:50 +0000

    Custom Cursor

--- static/he.js
+++ static/he.js
@@ -1,6 +1,6 @@
 const options = {
   bottom: '64px', // default: '32px'
-  right: '32px', // default: '32px'
+  right: 'unset', // default: '32px'
   left: '32px', // default: 'unset'
   time: '0.5s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
